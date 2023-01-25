f8bc83b6977494e25fb171e60c4872feeeb6bd3e
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-01-25 16:56:45 -0400

    Inserção de Categorias em Teste

--- src/main/java/br/com/macedo/agenda/testes/TestarCriarCategoria.java
+++ src/main/java/br/com/macedo/agenda/testes/TestarCriarCategoria.java
@@ -1,4 +1,34 @@
 package br.com.macedo.agenda.testes;
 
+import br.com.macedo.agenda.dao.CategoriaDao;
+import br.com.macedo.agenda.modelo.Categoria;
+import br.com.macedo.agenda.util.JPAUtil;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
 public class TestarCriarCategoria {
+
+    public static void main(String[] args) {
+
+    EntityManager entityManager = JPAUtil.getEntityManager();
+
+        Categoria categoria = new Categoria("Amigos");
+        Categoria categoria1 = new Categoria("Familia");
+        Categoria categoria2 = new Categoria("Escola");
+        Categoria categoria3 = new Categoria("Trabalho");
+
+        CategoriaDao categoriaDao = new CategoriaDao(entityManager);
+
+        entityManager.getTransaction().begin();
+
+        categoriaDao.cadastrarCategoria(categoria);
+        categoriaDao.cadastrarCategoria(categoria1);
+        categoriaDao.cadastrarCategoria(categoria2);
+        categoriaDao.cadastrarCategoria(categoria3);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 }
